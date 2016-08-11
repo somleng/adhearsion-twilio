94370b077d566c082d0b2532e86d7fde3ae0d3a3
David Wilkie <dwilkie@gmail.com>
2016-08-11 11:11:07 +0700

    Use require_relative instead of require

--- lib/adhearsion/twilio.rb
+++ lib/adhearsion/twilio.rb
@@ -6,9 +6,9 @@ require "active_support/concern"
 require "active_support/core_ext/numeric/time"
 
 require_relative "twilio/util"
-require "adhearsion/twilio/version"
-require "adhearsion/twilio/plugin"
-require "adhearsion/twilio/controller_methods"
+require_relative "twilio/version"
+require_relative "twilio/plugin"
+require_relative "twilio/controller_methods"
 
 module Adhearsion
   module Twilio
