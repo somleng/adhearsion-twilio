1f9bc78928612b55d1c5755d0aca500f42171504
David Wilkie <dwilkie@gmail.com>
2017-08-01 19:27:23 +0700

    Turn of simplecov when testing locally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
-require 'simplecov'
-SimpleCov.start
+if ENV["START_SIMPLECOV"].to_i == 1
+  require 'simplecov'
+  SimpleCov.start
+end
 
 require 'adhearsion-twilio'
 
 RSpec.configure do |config|
-  Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
+  Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 end
